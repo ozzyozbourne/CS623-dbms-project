728e6fda2e10e36f327f612d490d0cd88712b3b1
ozzyozbourne <khanosaid726@gmail.com>
2024-04-13 19:39:33 -0400

    updated the project structure

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,9 +6,20 @@ repositories {
     mavenCentral()
 }
 
+
+val agent: Configuration by configurations.creating {
+    isCanBeConsumed = true
+    isCanBeResolved = true
+}
+
 dependencies {
+    agent(libs.aspectj)
+
+    testImplementation(platform(libs.allureBom))
+    testImplementation("io.qameta.allure:allure-testng")
     testImplementation(libs.testng)
-    implementation("org.postgresql:postgresql:42.7.3")
+
+    implementation(libs.postgres)
 }
 
 java {
@@ -21,9 +32,29 @@ application {
     mainClass.set("project.App")
 }
 
+
 tasks.named<Test>("test") {
+    jvmArgs = listOf(
+        "-javaagent:${agent.singleFile}"
+    )
     useTestNG()
     testLogging {
         events("PASSED", "SKIPPED", "FAILED", "STANDARD_OUT", "STANDARD_ERROR")
+    }
+}
+
+tasks.register("testReport") {
+    dependsOn("test")
+    doLast {
+        val buildDir = layout.buildDirectory.dir("allure-results").get().asFile
+        buildDir.mkdirs()
+
+        val reportFile = File(buildDir, "environment.properties")
+        val osVersion = System.getProperty("os.version")
+        val osName = System.getProperty("os.name")
+        val javaVersion = System.getProperty("java.version")
+
+        reportFile.writeText("Platform = $osName : $osVersion\nJava = $javaVersion\n")
+        println("Test report generated at: ${reportFile.absolutePath}")
     }
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -2,5 +2,22 @@ plugins {
     id("org.gradle.toolchains.foojay-resolver-convention") version "0.7.0"
 }
 
+buildCache {
+    local {
+        directory = File(rootDir, "build-cache")
+        removeUnusedEntriesAfterDays = 30
+    }
+}
+
+dependencyResolutionManagement {
+    versionCatalogs {
+        create("libs") {
+            library("allureBom", "io.qameta.allure:allure-bom:2.24.0")
+            library("aspectj", "org.aspectj:aspectjweaver:1.9.20.1")
+            library("postgres", "org.postgresql:postgresql:42.7.3")
+        }
+    }
+}
+
 rootProject.name = "cs623"
 include("app")
